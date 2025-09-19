0762b6ee3217d0b4de0ca8bd92e224281613e4d9
Muhammad Taha Kamran <taharajput33@yahoo.com>
2025-09-19 19:26:27 +0500

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val p1 = Person(name = "Ali")
+    val p2 = Person(name = "Bilal", age = 23)
+    val p3 = Person (name = "Taha", age = 32, email = "taharajput33@yahoo.com")
+    p1.introduce()
+    p2.introduce()
+    p3.introduce()
+}
+class Person(val name: String){
+    var age: Int = 0
+    var email: String = ""
+    constructor(name: String, age: Int): this (name){
+        this.age = age
+    }
+    constructor(name: String, age: Int, email: String) : this (name, age){
+        this.email = email
+    }
+    fun introduce() {
+        println("Hi, My age is $age and email address is $email")
+    }
+}
---
+++ src/practice.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+}
+class Mathutils{
+    companion object{
+        const val PI = 3.14159
+        fun calculateCircleArea(radius: Double) : Double{
+            return PI*radius*radius
+        }
+    }
+}
